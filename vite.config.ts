db7b8b071ff953261b47597dfdab2276d7edf312
dr666m1 <skndr666m1@gmail.com>
2024-01-07 10:49:24 +0900

    show downloaded files

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { crx } from "@crxjs/vite-plugin";
-import manifest from "./manifest.json";
+import package_ from "./package.json";
+
+async function manifest() {
+  return {
+    manifest_version: 3,
+    name: "Copy File to Clipboard",
+    version: package_.version,
+    action: {
+      default_popup: "index.html",
+    },
+    permissions: ["downloads"],
+  };
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
